e0e33f3acb901715e25d228065de2961d62c8f7c
Karthick C <82097946+karthikiki@users.noreply.github.com>
2023-03-13 12:33:15 +0530

    Login using DOM

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function sum(){
+let fName= document.getElementById("fName").value
+console.log(fName);
+let mNAME= document.getElementById("mName").value
+console.log(mNAME);
+let lName= document.getElementById("lName").value
+console.log(lName);
+let Email= document.getElementById("Email").value
+console.log(Email);
+let pass= document.getElementById("Password").value
+console.log(pass);
+}
